86e7aaf7f9fe7f198e394e11f0e959c14cfc0084
Andres Antonio Castro Beltran <andres435b@gmail.com>
2026-01-30 21:12:08 -0700

    feat: :tada: Added project structure

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	""
+)
+
+type photo struct {
+	id int
+	url string
+	description string
+}
+
+func main() {
+	
+}
+
+// GET /headphones 
+func getHeadphones(c gin.Context) {
+	
+}
