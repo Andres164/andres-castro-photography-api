b0b81ce5b6363ef5e0602e38b806c410e2b9629b
Andres Antonio Castro Beltran <andres435b@gmail.com>
2026-04-05 22:57:45 -0700

    refactor: :construction: Started migration to Huma with gin integration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,11 +6,14 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humagin"
 )
 
 func main() {
 	database.InitDB()
 	r := gin.Default()
+	api := humagin.New(r, huma.DefaultConfig("Andres Castro photography API", "0.1.0"))
 
 	r.GET("/photos", handlers.GetPhotos)
 	r.GET("/photos/:id", handlers.GetPhotoById)
