898a6bca090705b1c593574ae4eb38f31b214c7d
Andres Antonio Castro Beltran <andres435b@gmail.com>
2026-07-15 23:19:30 -0700

    feat: :sparkles: Adds Upload method to StorageService, uploads an object to the Cloud Storage Bucket
    Tested and working correctly

--- internal/services/storage.go
+++ internal/services/storage.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"io"
 
 	"cloud.google.com/go/storage"
 )
@@ -25,4 +26,27 @@ func NewStorageClient(ctx context.Context) (*storage.Client, error) {
 func (s *StorageService) HealthCheck(ctx context.Context) error {
 	_, err := s.bucket.Attrs(ctx)
 	return err
+}
+
+func (s *StorageService) Upload(
+    ctx context.Context,
+    reader io.Reader,
+    objectName string,
+) (string, error) {
+	objectH := s.bucket.Object(objectName)
+	writer := objectH.NewWriter(ctx)
+
+	_, err := io.Copy(writer, reader)
+	if err != nil {
+		return "", err
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	objectUrl := "http://url/photos/" + objectName
+
+	return objectUrl, nil
 }
